fe95589bb069587a7c2b937ef4defcced5e135dd
Samuel Foltran <contato@samuelfoltran.com>
2022-08-28 15:02:59 -0300

    Created User() component, with initial implementation.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import ReactDOM from 'react-dom/client';
 
-function Foo(){
+function User(props){
+
   return(
-    "I'm Ready"
+    <div>
+      <img style={ {width: 50} } src={props.user.picture} />
+      <p>{props.user.name} / {props.user.age}</p>
+    </div>
   );
+  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const user = {
+  name: "Julia",
+  age: 25,
+  picture: "pictures/julia.png"
+}
+
 root.render(
-  <Foo />
+  <User user={user} />
 );
